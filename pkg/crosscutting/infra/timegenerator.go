481cccf041dc37272c71e06d83d7e5651b1457ba
hinoguma <noguchi.kohei.0904@gmail.com>
2026-03-14 00:39:48 +0900

    crosscutting (utils)

---
+++ pkg/crosscutting/infra/timegenerator.go
@@ -0,0 +1,36 @@
+package infra
+
+import (
+	"app/pkg/crosscutting/timer"
+	"time"
+)
+
+var jst = time.FixedZone("Asia/Tokyo", 9*60*60)
+
+type basedOnPlaceTimeGenerator struct {
+	tz time.Location
+}
+
+func (t basedOnPlaceTimeGenerator) Now() time.Time {
+	return time.Now().In(&t.tz)
+}
+
+func (t basedOnPlaceTimeGenerator) GetTz() time.Location {
+	return t.tz
+}
+
+func (t basedOnPlaceTimeGenerator) NowTs() timer.UnixTimestamp {
+	return timer.UnixTimestamp(time.Now().In(&t.tz).Unix())
+}
+
+func (t basedOnPlaceTimeGenerator) NowTsMills() timer.UnixTimestampMillis {
+	return timer.UnixTimestampMillis(time.Now().In(&t.tz).UnixMilli())
+}
+
+func NewTimeGenerator(tz time.Location) timer.TimeGenerator {
+	return &basedOnPlaceTimeGenerator{tz: tz}
+}
+
+func NewJstTimeGenerator() timer.TimeGenerator {
+	return &basedOnPlaceTimeGenerator{tz: *jst}
+}
---
+++ pkg/crosscutting/timer/timegenerator.go
@@ -0,0 +1,59 @@
+package timer
+
+import (
+	"time"
+)
+
+var globalTimeGenerator TimeGenerator = NewStdTimeGenerator()
+
+func SetGlobalTimeGenerator(tg TimeGenerator) {
+	globalTimeGenerator = tg
+}
+
+type TimeGenerator interface {
+	GetTz() time.Location
+	Now() time.Time
+	NowTs() UnixTimestamp
+	NowTsMills() UnixTimestampMillis
+}
+
+func NowTs() UnixTimestamp {
+	return globalTimeGenerator.NowTs()
+}
+
+func Now() time.Time {
+	return globalTimeGenerator.Now()
+}
+
+func NowTsMills() UnixTimestampMillis {
+	return globalTimeGenerator.NowTsMills()
+}
+
+func NowYmd_Hms_ms() Ymd_Hms_ms {
+	return NowTsMills().Ymd_Hms_ms()
+}
+
+type StdTimeGenerator struct {
+	tz time.Location
+}
+
+func NewStdTimeGenerator() TimeGenerator {
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
+	return &StdTimeGenerator{tz: *jst}
+}
+
+func (t StdTimeGenerator) GetTz() time.Location {
+	return t.tz
+}
+
+func (t StdTimeGenerator) Now() time.Time {
+	return time.Now().In(&t.tz)
+}
+
+func (t StdTimeGenerator) NowTs() UnixTimestamp {
+	return UnixTimestamp(time.Now().In(&t.tz).Unix())
+}
+
+func (t StdTimeGenerator) NowTsMills() UnixTimestampMillis {
+	return UnixTimestampMillis(time.Now().In(&t.tz).UnixMilli())
+}
