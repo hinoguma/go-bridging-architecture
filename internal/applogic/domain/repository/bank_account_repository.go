19afba7ace3b7d444df15592b99145096aee9084
hinoguma <noguchi.kohei.0904@gmail.com>
2026-03-24 14:54:54 +0900

    types

--- internal/applogic/domain/repository/bank_account_repository.go
+++ internal/applogic/domain/repository/bank_account_repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 type BankAccountRepository interface {
-	Get(ctx context.Context, id model.BankAccountID, optionaltFuncs ...model.DBOperationOptionalFunc) (model.BankAccount, error)
-	Create(ctx context.Context, account model.BankAccount, optionaltFuncs ...model.DBOperationOptionalFunc) (model.BankAccount, error)
-	Update(ctx context.Context, request model.UpdateBankAccountRequest, optionaltFuncs ...model.DBOperationOptionalFunc) error
+	Get(ctx context.Context, id model.BankAccountID, optionalFuncs ...model.DBOperationOptionalFunc) (model.BankAccount, error)
+	Create(ctx context.Context, account model.BankAccount, optionalFuncs ...model.DBOperationOptionalFunc) (model.BankAccount, error)
+	Update(ctx context.Context, request model.UpdateBankAccountRequest, optionalFuncs ...model.DBOperationOptionalFunc) error
 }
--- internal/applogic/domain/repository/transaction_record_repository.go
+++ internal/applogic/domain/repository/transaction_record_repository.go
@@ -6,6 +6,6 @@ import (
 )
 
 type TransactionRecordRepository interface {
-	Get(ctx context.Context, id model.TransactionRecordID, optionaltFuncs ...model.DBOperationOptionalFunc) (model.TransactionRecord, error)
-	Create(ctx context.Context, account model.TransactionRecord, optionaltFuncs ...model.DBOperationOptionalFunc) (model.TransactionRecord, error)
+	Get(ctx context.Context, id model.TransactionRecordID, optionalFuncs ...model.DBOperationOptionalFunc) (model.TransactionRecord, error)
+	Create(ctx context.Context, account model.TransactionRecord, optionalFuncs ...model.DBOperationOptionalFunc) (model.TransactionRecord, error)
 }
